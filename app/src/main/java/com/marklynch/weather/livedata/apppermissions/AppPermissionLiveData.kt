735710421022ffdda8b7e13c13a94fd2971cbeab
Mark Lynch <lynchm3@tcd.ie>
2019-07-12 11:07:32 +0100

    Refresh permission live data when new observer added

--- app/src/main/java/com/marklynch/weather/livedata/apppermissions/AppPermissionLiveData.kt
+++ app/src/main/java/com/marklynch/weather/livedata/apppermissions/AppPermissionLiveData.kt
@@ -4,7 +4,9 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 
 enum class AppPermissionState { Granted, Denied }
 
@@ -15,20 +17,24 @@ class AppPermissionLiveData(
     private val permissionToCheck: String
 ) : LiveData<AppPermissionState>() {
 
+    override fun observe(owner: LifecycleOwner, observer: Observer<in AppPermissionState>) {
+        super.observe(owner, observer)
+        checkPermission()
+    }
 
     override fun onActive() {
         checkPermission()
     }
 
     private fun checkPermission() {
-            if (isPermissionGranted(context, permissionToCheck))
-                postValue(AppPermissionState.Granted)
-            else
-                postValue(AppPermissionState.Denied)
+        if (isPermissionGranted(context, permissionToCheck))
+            postValue(AppPermissionState.Granted)
+        else
+            postValue(AppPermissionState.Denied)
     }
-}
 
-fun isPermissionGranted(context:Context, permissionToCheck:String): Boolean = ActivityCompat.checkSelfPermission(
-    context,
-    permissionToCheck
-) == PackageManager.PERMISSION_GRANTED
+    private fun isPermissionGranted(context: Context, permissionToCheck: String): Boolean = ActivityCompat.checkSelfPermission(
+        context,
+        permissionToCheck
+    ) == PackageManager.PERMISSION_GRANTED
+}
